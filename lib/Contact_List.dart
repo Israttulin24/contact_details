b677063da98809dcb4fd8c51b6217fd245488a1e
Israt Jahan Tulin <135654667+Israttulin24@users.noreply.github.com>
2023-07-04 01:15:39 +0600

    Contact_List.dart

--- lib/Contact_List.dart
+++ lib/Contact_List.dart
@@ -11,156 +11,7 @@ class Contact extends StatefulWidget {
 
 class ContactView extends State<Contact>{
 
-   List ToDoList1 = [{1}];
-  List ToDoList2 = [{'doe23@gmail.com'},'jane11@gmail.com','alice32@gmail.com'];
-  List ToDoList3 = ['3456786789','345678900','908765443'];
-  // String item1 = "";
-  // String item2= "";
-  // String item3= "";
-  //
-  // MyInputOnChange1(content1){
-  //   setState(() {
-  //     item1 = content1;
-  //   });
-  // }
-  // MyInputOnChange2(content2){
-  //   setState(() {
-  //     item2 = content2;
-  //   });
-  // }
-  // MyInputOnChange3(content3){
-  //   setState(() {
-  //     item3 = content3;
-  //   });
-  // }
-  //
-  //
-  // AddItem1(){
-  //   setState(() {
-  //     ToDoList1.add({"item":item1});
-  //   });
-  // }
-  //
-  // AddItem2(){
-  //   setState(() {
-  //     ToDoList2.add({"item":item2});
-  //   });
-  // }
-  //
-  // AddItem3(){
-  //   setState(() {
-  //     ToDoList3.add({"item":item3});
-  //   });
-  // }
-  //
-  // RemoveItem1(index){
-  //   setState(() {
-  //     ToDoList1.removeAt(index);
-  //   });
-  // }
-  //
-  // RemoveItem2(index){
-  //   setState(() {
-  //     ToDoList2.removeAt(index);
-  //   });
-  // }
-  //
-  // RemoveItem3(index){
-  //   setState(() {
-  //     ToDoList3.removeAt(index);
-  //   });
-  // }
-  //
-  //
-  // MyAlertDialog(context){
-  //   return showDialog(
-  //       context: context,
-  //       builder: (BuildContext context){
-  //         return Expanded(
-  //             child: AlertDialog(
-  //               title: Text('Add Task'),
-  //               content: Container(
-  //                   height: 240,
-  //                   child: SingleChildScrollView(
-  //                     child: Column(
-  //                         children: [
-  //                           SizedBox(
-  //                             child: TextField(onChanged: (content1){MyInputOnChange1(content1);},decoration: InputDecoration(labelText:'Title',enabledBorder: OutlineInputBorder(borderSide: BorderSide(width: 2, color: Colors.grey))),),
-  //                           ),
-  //                           Padding(padding: EdgeInsets.all(5)),
-  //
-  //                           SizedBox(
-  //                             child: TextField(onChanged: (content2){MyInputOnChange2(content2);},decoration: InputDecoration(labelText:'Description',contentPadding: EdgeInsets.fromLTRB(13, 40, 40, 40),enabledBorder: OutlineInputBorder(borderSide: BorderSide(width: 2, color: Colors.grey))),),
-  //                           ),
-  //                           Padding(padding: EdgeInsets.all(5)),
-  //
-  //                           SizedBox(
-  //                             child: TextField(onChanged: (content3){MyInputOnChange3(content3);},decoration: InputDecoration(labelText:'Days Required',enabledBorder: OutlineInputBorder(borderSide: BorderSide(width: 2, color: Colors.grey))),),
-  //                           ),
-  //
-  //                         ]
-  //
-  //                     ),
-  //                   )
-  //
-  //               ),
-  //
-  //               actions: [
-  //                 TextButton(onPressed: (){AddItem1();AddItem2();AddItem3();Navigator.of(context).pop();}, child: Text('Save'))
-  //               ],
-  //             )
-  //         );
-  //       }
-  //
-  //   );
-  // }
-
-  myTaskBottomSheet(){
-    return SizedBox(
-      width: 500,
-      height: 200,
-      child: BottomSheet(
-          onClosing: (){Navigator.of(context).restorationId;},
-          builder:(BuildContext){
-            return ListView.builder(
-                itemCount: ToDoList1.length,
-                itemBuilder: (context,index){
-                  return Container(
-                    padding: EdgeInsets.only(left: 20),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Contact Details',style: TextStyle(fontSize:20,fontWeight:FontWeight.bold),),
-                        Padding(padding: EdgeInsets.only(bottom: 15)),
-                        Text('Name: '+ToDoList1[index]["item"].toString()),
-                        Text('Email: '+ToDoList2[index]["item"].toString()),
-                        Text('Phone Number: '+ToDoList3[index]["item"].toString()),
-
-
-
-                      ],
-                    ),
-                  );
-
-
-                }
-
-            );
-          }
-
-      ),
-
-
-
-
-
-    );
-
-
-  }
-
-
-
+  List ToDoList1 = [{1}];
 
   @override
   Widget build(BuildContext context) {
@@ -174,9 +25,6 @@ class ContactView extends State<Contact>{
           itemCount: ToDoList1.length,
           itemBuilder: (context,index){
             return GestureDetector(
-
-              onTap: (){showBottomSheet(context: context, builder: (BuildContext){return myTaskBottomSheet();});},
-
               child: Card(
                 elevation: 0,
                 child: Column(
@@ -184,18 +32,89 @@ class ContactView extends State<Contact>{
                     ListTile(
                       title: Text('John Doe'),
                       onTap: (){
-                        showBottomSheet(context: context, builder: (BuildContext){return
-                          myTaskBottomSheet();});
+                        showBottomSheet(context: context, builder: (BuildContext){
+                        return SizedBox(
+                          width: 500,
+                          height: 200,
+                          child: BottomSheet(
+                              onClosing: (){Navigator.of(context).restorationId;},
+                              builder:(BuildContext){
+                                      return Container(
+                                        padding: EdgeInsets.only(left: 20),
+                                        child: Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: [
+                                            Text('Contact Details',style: TextStyle(fontSize:20,fontWeight:FontWeight.bold),),
+                                            Padding(padding: EdgeInsets.only(bottom: 15)),
+                                            Text('Name: John Doe'),
+                                            Text('Email: doe23@gmail.com'),
+                                            Text('Phone Number: 546789'),
+                                          ],
+                                        ),
+                                      );
+                                    }
+                          ),
+                        );});
                       },
                     ),
 
                     ListTile(
                       title: Text('Jane Smith'),
+                      onTap: (){
+                        showBottomSheet(context: context, builder: (BuildContext){
+                          return SizedBox(
+                            width: 500,
+                            height: 200,
+                            child: BottomSheet(
+                                onClosing: (){Navigator.of(context).restorationId;},
+                                builder:(BuildContext){
+                                  return Container(
+                                    padding: EdgeInsets.only(left: 20),
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text('Contact Details',style: TextStyle(fontSize:20,fontWeight:FontWeight.bold),),
+                                        Padding(padding: EdgeInsets.only(bottom: 15)),
+                                        Text('Name: Jane Smith'),
+                                        Text('Email: smith23@gmail.com'),
+                                        Text('Phone Number: 946789'),
+                                      ],
+                                    ),
+                                  );
+                                }
+                            ),
+                          );});
+                      },
                     ),
 
                     ListTile(
                       title: Text('Alice Johnson'),
-                    )
+                      onTap: (){
+                        showBottomSheet(context: context, builder: (BuildContext){
+                          return SizedBox(
+                            width: 500,
+                            height: 200,
+                            child: BottomSheet(
+                                onClosing: (){Navigator.of(context).restorationId;},
+                                builder:(BuildContext){
+                                  return Container(
+                                    padding: EdgeInsets.only(left: 20),
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text('Contact Details',style: TextStyle(fontSize:20,fontWeight:FontWeight.bold),),
+                                        Padding(padding: EdgeInsets.only(bottom: 15)),
+                                        Text('Name: Alice Johnson'),
+                                        Text('Email: alice@gmail.com'),
+                                        Text('Phone Number: 646789'),
+                                      ],
+                                    ),
+                                  );
+                                }
+                            ),
+                          );});
+                      },
+                    ),
 
                   ],
                 ),
